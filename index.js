7df660427ddf494f1c0bce1e700c2c171ee088f0
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-05-03 12:42:59 +0600

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const chefs = require('./data/chefs.json');
 
@@ -9,7 +11,15 @@ app.get('/', (req, res) => {
   res.send('Server Exquisite Cuisine is running');
 })
 
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
 
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(chef => chef.id === id);
+    res.send(selectedChef);
+})
 
 app.listen(port, () => {
   console.log(`Exquisite Cuisine server listening on port ${port}`)
